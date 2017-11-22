295a273132b180d51eea4e27a6cb29a3984e6208
aitoehigie <aitoehigie@gmail.com>
2017-11-22 18:34:23 +0100

    Birthday_Cake_Candles

---
+++ birthdaycakecandles.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+
+def birthdayCakeCandles(n, ar):
+    # Complete this function
+    ar.sort()
+    return ar.count(ar[-1])
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = birthdayCakeCandles(n, ar)
+print(result)
+
